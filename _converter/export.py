712aee620ef212b70b097ce265d6319435f57904
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2023-07-26 15:10:41 +0100

    Add files via upload

--- _converter/export.py
+++ _converter/export.py
@@ -2,19 +2,21 @@ from os import listdir, remove
 from shutil import rmtree
 from time import sleep
 
-for fname in ["node_modules", "__pycache__"]:
+for fname in ["node_modules", "__pycache__", "output", "final_output"]:
     try:
         rmtree("{}/".format(fname))
     except:
         pass
 
-
-for fol in ["input", "output"]:
-    try:
-        for f in listdir("{}/".format(fol)):
-            remove("{}/{}".format(fol, f))
-    except:
-        pass
+try:
+    in_folder = "input/"
+    for f in listdir(in_folder):
+        remove("{}{}".format(in_folder, f))
+    f = open(in_folder+"placeholder", 'w')
+    f.write("placeholder")
+    f.close()
+except:
+    pass
 
 print("Done")
 sleep(1)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-from os import listdir, remove
+from os import listdir, mkdir
 from os.path import isfile, join
 from subprocess import call
 from time import sleep
@@ -11,19 +11,36 @@ def exception(msg):
     raise Exception()
 
 # generate formatted string from q details
-def parse_q(q, ans):
+def parse_q(q, correct_raw, ans):
+    correct = []
+    if correct_raw=="":
+        correct_raw = "N/A"
+    elif correct_raw=="0":
+        correct_raw="ERROR"
+    else:
+        correct = correct_raw.split(",")
+        try:
+            correct = [int(q_i) for q_i in correct]
+        except:
+            exception("Correct column format is not valid: {}".format(correct_raw))
+    
+    
     q = q.replace("```", "\n```").replace("\n\n```", "\n```")
     output = "\n"+q
     for i, a in enumerate(ans):
         bold = "  "
-        if i==0:
+        if i+1 in correct:
             bold = "**"
         output += "\n{}. {}{}{}".format(i+1, bold, a, bold)
+    
+    output += "\n\nCorrect: {}".format(correct_raw)
+
     return output
 
 # installs node modules if required and joins processed csvs 
-def init(dirname, outname):
+def init(dirname, outname, out_path):
     call("_converter\init.bat")
+    mkdir(out_path)
 
     # join csvs
     fnames = [f for f in listdir(dirname) if isfile(join(dirname, f))]
@@ -50,18 +67,23 @@ def init(dirname, outname):
 
 # input/output for the program
 in_path="_converter/output/"
-out_path="output/"
+out_path="_converter/final_output/"
 
 # user's input
 user_path = "_converter/input/"
 user_files = listdir(user_path)
+try:
+    user_files.remove("placeholder")
+except:
+    pass
+
 if len(user_files)!=1:
     exception("Must provide exactly 1 input file. You've given {}.".format(len(user_files)))
 user_fname = user_files[0]
 
 # create input csv for program
 joined_fname = out_path+"[joined] "+ user_fname
-init(in_path, joined_fname)
+init(in_path, joined_fname, out_path)
 
 # make data
 f = open(joined_fname, "r")
@@ -74,7 +96,7 @@ for line in csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, s
         continue
     i+=1
     output += "\n### Question {}".format(i)
-    output += parse_q(line[1], line[3:])
+    output += parse_q(line[1], line[2], line[3:])
     output += "\n"
 
 output = output.replace('\u2713', '') # remove rogue trailing character, strip() doesn't work
@@ -84,5 +106,4 @@ f = open(out_path+"[parsed] " + user_fname.replace(".csv", ".md"), "w")
 f.write(output)
 f.close()
 
-# remove(joined_fname)
 sleep(1)
